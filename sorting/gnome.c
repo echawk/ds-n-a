a4c2ec85429773eb8d162b643fae53eba4ac88c7
Spencer Gannon <spencer.gannon@valpo.edu>
2023-01-30 13:34:18 -0600

    Added .gitignore to ignore the executables created by build script

---
+++ sorting/gnome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "array.h"
+
+void gnome_sort(Arr_T arr){
+	int pos = 0;
+	
+	while(pos < arr.size){
+		if((pos == 0) || (arr.arr[pos] >= arr.arr[pos - 1])){
+			pos += 1;
+		}
+		else{
+			swap(pos, pos - 1, arr);
+			pos -= 1;
+		}
+	}
+
+	return;
+}
+
+int main(void){
+	Arr_T test_arr = make_Arr(10);
+	populate_Arr(test_arr);
+
+	printf("Array before it is sorted: ");
+	print_Arr(test_arr);
+
+	gnome_sort(test_arr);
+
+	printf("Array after it is gnome sorted: ");
+	print_Arr(test_arr);
+
+	return EXIT_SUCCESS;
+}
