b346f6d419e5e87d2c7a555b964f296efa74954b
Ethan <ethan.hawk@valpo.edu>
2023-01-30 23:38:10 -0600

    sorting/array.h: add 'within_Arr()' & 'largestIndIn_Arr'

--- sorting/array.h
+++ sorting/array.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,27 @@ void populate_Arr(Arr_T A) {
   for (i = 0; i < A.size; i++) {
     A.arr[i] = rand() % A.size;
   }
+}
+
+bool within_Arr(int val, Arr_T A) {
+  int i = 0;
+  for (i = 0; i < A.size; i++) {
+    if (val == A.arr[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int largestIndIn_Arr(Arr_T A) {
+  int li = 0;
+  int i = 0;
+  for (i = 0; i < A.size; i++) {
+    if (A.arr[i] > A.arr[li]) {
+      li = i;
+    }
+  }
+  return li;
 }
 
 void print_Arr(Arr_T A) {
