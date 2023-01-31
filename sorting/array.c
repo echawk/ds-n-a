3c88c0c9485016c16fe9724c0e6f96a2072d69ef
Ethan <ethan.hawk@valpo.edu>
2023-01-31 00:54:28 -0600

    sorting/array.c: add more methods to array.c

--- sorting/array.c
+++ sorting/array.c
@@ -1,4 +1,5 @@
 #include "array.h"
+#include <stdlib.h>
 
 Arr_T make_Arr(int size) {
   return (Arr_T){.size = size, .arr = calloc(size, sizeof(int))};
@@ -8,6 +9,25 @@ void populate_Arr(Arr_T A) {
   int i = 0;
   for (i = 0; i < A.size; i++) {
     A.arr[i] = rand() % A.size;
+  }
+}
+
+void sorted_populate_Arr(Arr_T A) {
+  int i = 0;
+  for (i = 0; i < A.size; i++) {
+    A.arr[i] = i + 1;
+  }
+}
+
+// Thank you once again stackoverflow.
+// https://stackoverflow.com/questions/6127503/shuffle-array-in-c#6127606
+void shuffle_Arr(Arr_T A) {
+  if (A.size > 1) {
+    int i;
+    for (i = 0; i < A.size; i++) {
+      int j = i + rand() / (RAND_MAX / (A.size - i) + 1);
+      swap(i, j, A);
+    }
   }
 }
 
