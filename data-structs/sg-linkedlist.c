7aae77669ff14e6eaea8e3d3017dff3226153ff8
Spencer Gannon <spencer.gannon@valpo.edu>
2023-02-08 09:28:36 -0600

    Changed pointers to character pointers in linked list

--- data-structs/sg-linkedlist.c
+++ data-structs/sg-linkedlist.c
@@ -6,7 +6,7 @@
 
 #define DEBUG_STATEMENTS 1
 
-node_t* node_init(int _data, node_t* _next){
+node_t* node_init(int _data, char* _next){
 	node_t* ret = (node_t*)calloc(1, sizeof(node_t));
 
 	ret->data = _data; 
@@ -33,30 +33,30 @@ node_t* node_search(node_t** head_ref, int data, int instance){
 			ret = ptr;
 			instances_passed++;
 		}
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 	}
 
 	return ret;	//this will return the last instance or NULL which is valid
 }
 // returns 
-bool node_delete(node_t** head_ref, node_t* key){
+bool node_delete(node_t** head_ref, char* key){
 	node_t* ptr = *head_ref;
 	node_t* last_node = NULL;
 
 	while(ptr != NULL){
-		if(ptr == key){
+		if(ptr == (node_t*)key){
 			if(last_node != NULL){
 				last_node->next = ptr->next;
 				ptr->next = NULL;
 				free(ptr);	//free the node we're no longer using
 			}
 			else{
-				*head_ref = ptr->next;
+				*head_ref = (node_t*)ptr->next;
 			}
 			return true;
 		}
 		last_node = ptr;
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 	}
 
 	return false;
@@ -75,12 +75,12 @@ bool node_delete_at_place(node_t** head_ref, int index){
 				free(ptr);	//free the node we're no longer using
 			}
 			else{
-				*head_ref = ptr->next;
+				*head_ref = (node_t*)ptr->next;
 			}
 			return true;
 		}
 		last_node = ptr;
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 		current_index++;
 	}
 
@@ -88,7 +88,7 @@ bool node_delete_at_place(node_t** head_ref, int index){
 }
 
 void node_add_to_front(node_t** head_ref, int new_data){
-	node_t* new_node = node_init(new_data, *head_ref);
+	node_t* new_node = node_init(new_data, (char*)*head_ref);
 	*head_ref = new_node;
 
 	return;
@@ -99,10 +99,10 @@ void node_add_to_end(node_t** head_ref, int new_data){
 	node_t* ptr = *head_ref;
 
 	while(ptr->next != NULL){
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 	}
 
-	ptr->next = new_node;
+	ptr->next = (char*)new_node;
 
 	return;
 }
@@ -116,7 +116,7 @@ int node_get_at_end(node_t** head_ref){
 	node_t* ptr = *head_ref;
 
 	while(ptr->next != NULL)
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 
 	return ptr->data;
 }
@@ -126,26 +126,26 @@ void node_print(node_t* head){
 	
 	while(ptr != NULL){
 		printf("%d ", ptr->data);
-		ptr = ptr->next;
+		ptr = (node_t*)ptr->next;
 	}
 	printf("\n");
 
 	return;
 }
 
-// int main(void){
-// 	node_t* head = calloc(1, sizeof(node_t));
+int main(void){
+	node_t* head = calloc(1, sizeof(node_t));
 
-// 	node_add_to_front(&head, 2);
-// 	node_add_to_front(&head, 2);
-// 	node_add_to_front(&head, 3);
-// 	node_add_to_front(&head, 2);
-// 	node_add_to_front(&head, 4);
-// 	node_add_to_front(&head, 0);
-// 	node_add_to_end(&head, 0);
-// 	node_print(head);
-// 	node_delete_at_place(&head, 1);
-// 	node_print(head);
+	node_add_to_front(&head, 2);
+	node_add_to_front(&head, 2);
+	node_add_to_front(&head, 3);
+	node_add_to_front(&head, 2);
+	node_add_to_front(&head, 4);
+	node_add_to_front(&head, 0);
+	node_add_to_end(&head, 0);
+	node_print(head);
+	node_delete_at_place(&head, 1);
+	node_print(head);
 
-// 	exit(EXIT_SUCCESS);
-// }
+	exit(EXIT_SUCCESS);
+}
