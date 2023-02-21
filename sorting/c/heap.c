459a4e19d721fb918aff6bd6b0f4a5ab663ac005
C-Garcia19 <christian.garcia@valpo.edu>
2023-02-21 12:46:20 -0600

    added the sort function so heap sort can be visualized

--- sorting/c/heap.c
+++ sorting/c/heap.c
@@ -1,33 +1,33 @@
+#include "array.h"
 #include <stdio.h>
-#include "array.c"
 
-void heapify(Arr_T A, int N, int i){
-    int largest = i;
-    int left = (2 * i) + 1;
-    int right = (2* i) + 2;
-    if (left < N && A.arr[left] > A.arr[largest]){
-        largest = left;
-    }
-    if (right < N && A.arr[right] > A.arr[largest]){
-        largest = right;
-    }
-    if (largest != i){
-        swap(i, largest, A);
-        heapify(A, N, largest);
-    }
+void heapify(Arr_T A, int N, int i) {
+  int largest = i;
+  int left = (2 * i) + 1;
+  int right = (2 * i) + 2;
+  if (left < N && A.arr[left] > A.arr[largest]) {
+    largest = left;
+  }
+  if (right < N && A.arr[right] > A.arr[largest]) {
+    largest = right;
+  }
+  if (largest != i) {
+    swap(i, largest, A);
+    heapify(A, N, largest);
+  }
 }
 
-void heapSort(Arr_T A, int N){
-    for(int i = (N / 2 - 1); i >= 0; i--){
-        heapify(A, N, i);
-    }
-    for(int j = (N - 1); j >= 0; j--){
-        swap(0, j, A);
-        heapify(A, j, 0);
-    }
+void heapSort(Arr_T A, int N) {
+  for (int i = (N / 2 - 1); i >= 0; i--) {
+    heapify(A, N, i);
+  }
+  for (int j = (N - 1); j >= 0; j--) {
+    swap(0, j, A);
+    heapify(A, j, 0);
+  }
 }
 
-
+void sort(Arr_T A) { heapSort(A, A.size); }
 
 #ifndef ASLIB
 int main(int argc, char *argv[]) {
