aabc5a264785efdfce67a7660b7acdb2f0fa8d98
C-Garcia19 <christian.garcia@valpo.edu>
2023-03-02 14:11:34 -0600

    fixed combine_Arrs in array.py

--- sorting/python/array.py
+++ sorting/python/array.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Arr_T:
     size: int
     arr: [int]
@@ -32,48 +33,43 @@ class Arr_T:
         return li
 
     def print_Arr(self):
-        print("[" + ', '.join(str(x) for x in self.arr) + "]", sep = ', ')
+        print("[" + ", ".join(str(x) for x in self.arr) + "]", sep=", ")
 
     def swap(self, ind1: int, ind2: int):
         if self.arr[ind1] != self.arr[ind2]:
             self.arr[ind1], self.arr[ind2] = self.arr[ind2], self.arr[ind1]
 
     def sorted_populate_Arr(self):
-        for i in range (self.size):
+        for i in range(self.size):
             self.arr[i] = i + 1
 
     def shuffle_Arr(self):
-        if (self.arr > 1):
+        if self.size > 1:
             for i in range(self.size):
                 j = i + random.randint((random.randint(max, max) / self.size - i)) + 1
                 self.swap(i, j)
 
-    #Does not work correctly yet
     def combine_Arrs(self, B):
-        N = self.Arr_T()
-        N = self.make_Arr(self.size + B.size)
+        array = [0] * (self.size + B.size)
         for i in range(self.size):
-            N.arr[i] = self.arr[i]
-        for j in range(self.size, N.size):
-            N.arr[j] = B.arr[j - self.size]
-
+            array[i] = self.arr[i]
+        for j in range(self.size, len(array)):
+            array[j] = B.arr[j - self.size]
+        N = self.make_Arr(self.size + B.size)
+        for k in range(len(array)):
+            self.arr[k] = array[k]
 
     def set_Value(self, value: int, ind: int):
         self.arr[ind] = value
 
 
-
-'''
+"""Main is here for testing if needed
 if __name__ == "__main__":
     array = Arr_T()
     array.make_Arr(50)
-    array2 = Arr_T()
-    array2.make_Arr(50)
     array.populate_Arr()
-    array2.populate_Arr()
     print("Array before combining: ")
     array.print_Arr()
-    array.combine_Arrs(array2)
     print("Array after combining: ")
     array.print_Arr()
-    '''
+"""
