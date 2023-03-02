a22b25e0f440ced857a8e6f5d592a0e73dc1a6ac
C-Garcia19 <christian.garcia@valpo.edu>
2023-03-02 14:01:08 -0600

    implemented counting sort in python

---
+++ sorting/python/counting.py
@@ -0,0 +1,37 @@
+import array
+
+
+def get_max(A) -> int:
+    max = A.arr[0]
+    for i in range(1, A.size):
+        if A.arr[i] > max:
+            max = A.arr[i]
+    return max
+
+
+def counting_sort(A):
+    output = [0] * A.size
+    max = get_max(A)
+    count = [0] * (max + 1)
+    for i in range(max + 1):
+        count[i] = 0
+    for j in range(A.size):
+        count[A.arr[j]] += 1
+    for k in range(1, max + 1):
+        count[k] += count[k - 1]
+    for n in range(A.size - 1, -1, -1):
+        output[count[A.arr[n]] - 1] = A.arr[n]
+        count[A.arr[n]] -= 1
+    for m in range(A.size):
+        A.arr[m] = output[m]
+
+
+if __name__ == "__main__":
+    array = array.Arr_T()
+    array.make_Arr(100)
+    array.populate_Arr()
+    print("Array before sorting: ")
+    array.print_Arr()
+    counting_sort(array)
+    print("Array after sorting: ")
+    array.print_Arr()
